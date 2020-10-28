ab923f3b8f3d0e0a4c227b2e1ff1c90d66ed8efb
你 <1945912279@qq.com>
2020-10-28 11:09:26 +0800

    1

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
-  .product product
+  .product-box
+
 </template>
 
 <script>
 
 export default {
-  
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    this.$http.get("http://localhost:8080/product.json").then(data=>{
+      console.log(data)
+    })
+  },
 }
 </script>
